737aff1a12385d5b97804ee8e7ce67f0ef729a8d
damianfe <damianrocio12@gmail.com>
2024-08-01 09:50:40 -0300

    fix: add types in context

---
+++ freelancer-timer-keeper/frontend/src/context/auth.types.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+export interface AuthContextType {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+export interface AuthProviderProps {
+  children: ReactNode;
+}
